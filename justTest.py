bec3e25af8a0d6cb29d5fea8843aaa10d6a27db1
zhouwei <1175178353@qq.com>
2021-06-02 15:50:13 +0800

    tt

--- justTest.py
+++ justTest.py
@@ -8,4 +8,4 @@ host=""
 # data={"sysParams":{"requestTime":1622443721000,"appKey":"8B6EA5A09B8DDAA9770EA340AE35D3BA","sign":"587A4626D4E6045E80A420363199C2ED"},"params":{"userName":"15906266923","password":"d9c37e748cd899191c661a7b0dbd62c3","isAlicloud":"1","deviceId":"46d7e35a80a9480f8771e30e8aea3dc1","systemContext":"/siweiApp","deviceType":1,"loginId":"yxyljg"}}
 # s.post(host+login_url,json.dumps(data),headers={"Content-Type": "application/json"}
 
-print("")
+print("hello,world")
