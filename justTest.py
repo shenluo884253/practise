5b8041cc5486596c3176dfce520ceee3d04fb7f8
zhouwei <1175178353@qq.com>
2021-06-02 15:05:49 +0800

    remove host

--- justTest.py
+++ justTest.py
@@ -3,7 +3,7 @@ import hashlib
 import requests
 s=requests.session()
 import json
-host="http://47.96.25.44:10010"
+host=""
 # login_url="/app/user/login"
 # data={"sysParams":{"requestTime":1622443721000,"appKey":"8B6EA5A09B8DDAA9770EA340AE35D3BA","sign":"587A4626D4E6045E80A420363199C2ED"},"params":{"userName":"15906266923","password":"d9c37e748cd899191c661a7b0dbd62c3","isAlicloud":"1","deviceId":"46d7e35a80a9480f8771e30e8aea3dc1","systemContext":"/siweiApp","deviceType":1,"loginId":"yxyljg"}}
 # s.post(host+login_url,json.dumps(data),headers={"Content-Type": "application/json"}
