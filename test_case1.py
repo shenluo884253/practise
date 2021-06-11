3e99b41d53b69651418761f60b898e0a8f381dcb
zhouwei <1175178353@qq.com>
2021-06-11 15:49:46 +0800

    test

---
+++ test_case1.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_hh():
+    print("hhzhao")
+
+
+def test_zw():
+    print('zw')
+
+
+class TestIsTrue:
+    def test_login(self):
+        print("login")
+
+    def test_upload(self):
+        print("upload")
+
+
